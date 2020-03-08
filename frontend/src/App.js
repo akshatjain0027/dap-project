814acd1de1633b4696d6ce933cdd0329f9f1e87f
Akshat <akshatjain0027@gmail.com>
2020-03-08 22:08:25 +0530

    added header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/header/header.component'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+  render(){
+    return (
+      <div className = 'App'>
+        <Header currentUser={this.state.currentUser}/>
+
+      </div>
+    )
+    
+  }
+  
 }
 
 export default App;
---
+++ frontend/src/components/header/header.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './header.styles.css'
+
+const Header = (currentUser) => (
+    <div className = 'header'>
+        <div className='header1'>
+            
+            <ul class="nav nav-pills">
+                <li role="presentation" class="active"><a href="#">Home</a></li>
+                <li role="presentation"><a href="#">Contact</a></li>
+                <li role="presentation"><a href="#">AboutUs</a></li>
+                <li role="presentation"><a href="#">News</a></li>
+
+            </ul>
+                
+        </div>
+        <div className='header2'>
+            
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">AskUSICT</a>
+                        <form class="navbar-form navbar-left" role="search">
+                          <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search"/>
+                          </div>
+                          <button type="submit" class="btn btn-default">Search</button>
+                        </form>
+                        <ul class="nav navbar-nav">
+
+                            <li><a href="#">Ask Question</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+        
+    </div>
+)
+
+export default Header;
