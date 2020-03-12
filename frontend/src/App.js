f67c028667acf0d77967774e351ea5f1214b1ea1
Akshat <akshatjain0027@gmail.com>
2020-03-13 00:26:41 +0530

    minor changes in header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,19 +17,8 @@ class App extends React.Component{
 
     this.state = {
       currentUser: null,
-      showModal:false
     }
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
-  }
-  
-  handleOpenModal () {
-    this.setState({ showModal: true });
-  }
-  
-  handleCloseModal () {
-    this.setState({ showModal: false });
-  }
+  } 
   
   render(){
     return (
