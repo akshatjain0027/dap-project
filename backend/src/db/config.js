79d8c4915cc8e28cc38f9e2d9f1bac9fb0f17567
PiyushKhurana <khuranapiyush27@gmail.com>
2020-03-17 20:10:45 +0530

    complete refactored

--- backend/src/db/config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/test-db-dap", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
-module.exports={};
