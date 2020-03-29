295cb5ac9d697ecca7c32d1faa9c6c5c23f8df60
PiyushKhurana <khuranapiyush27@gmail.com>
2020-03-29 12:59:00 +0530

    corrected class to className in header component

--- frontend/src/components/header/header.component.jsx
+++ frontend/src/components/header/header.component.jsx
@@ -1,99 +1,135 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import ReactModal from 'react-modal';
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactModal from "react-modal";
 
-import './header.styles.css'
-import FormInput from '../form-input/form-input.component';
+import "./header.styles.css";
+import FormInput from "../form-input/form-input.component";
 
-class Header extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            question:"",
-            showModal:false
-        }
-        this.handleOpenModal = this.handleOpenModal.bind(this);
-        this.handleCloseModal = this.handleCloseModal.bind(this);
-      }
-      
-    handleOpenModal () {
-        this.setState({ showModal: true });
-    }
-      
-    handleCloseModal () {
-        this.setState({ showModal: false });
-    }
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      question: "",
+      showModal: false
+    };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
 
-    handleChange = event => {
-        const {value, name} = event.target;
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
 
-        this.setState({[name]: value})
-    }
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
 
-    handleSubmit = async event => {
-        event.preventDefault();
-        const {question} = this.state;
-    }
+  handleChange = event => {
+    const { value, name } = event.target;
 
-    render(){
-        return(
-            <div className = 'header'>
-            <div className='header1'>
-                
-                <ul class="nav nav-pills">
-                    <li role="presentation" class="active"><Link to='/'>Home</Link></li>
-                    <li role="presentation" class="active"><Link to='/contact'>Contact</Link></li>
-                    <li role="presentation" class="active"><Link to='/about'>About AskUss</Link></li>
-                    <li role="presentation"><a href="#">News</a></li>
-                </ul>
-                    
-            </div>
-    
-            <div className='header2'>
-                <nav class="navbar navbar-default">
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <Link class="navbar-brand" to='/'>AskUSICT</Link>
-    
-                            <form class="navbar-form navbar-left" role="search">
-                              <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Search"/>
-                              </div>
-                              <button type="submit" class="btn btn-default">Search</button>
-                            </form>
-    
-                            <ul class="nav navbar-nav">
-    
-                                <li><a href="#" onClick={this.handleOpenModal}>Ask Question</a></li>
-                                <ReactModal isOpen={this.state.showModal} 
-                                    contentLabel="ask question modal"
-                                    className='Modal'
-                                    >
-                                 
+    this.setState({ [name]: value });
+  };
 
-                                    <h1>Ask Your Question Here!</h1>
-                                    <form action="" onSubmit={this.handleSubmit}>
-                                        <FormInput label="Question?" type="text" name='question' value={this.state.question} handleChange={this.handleChange} />
-                                        <FormInput label="Tags(Optional)" type="text" name='question' value={this.state.question} handleChange={this.handleChange} />
-                                        <button className='ask btn btn-primary' type='submit' onClick={this.handleCloseModal}>Ask</button>
-                                        <button className='cancel btn btn-warning' onClick={this.handleCloseModal}>Cancel</button>
+  handleSubmit = async event => {
+    event.preventDefault();
+    const { question } = this.state;
+  };
 
-                                    </form>
-                                
-                                    
-                                </ReactModal>
-                                
-                                <li><Link to="/login">SignIn</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-    
-            </div>
-            
+  render() {
+    return (
+      <div className="header">
+        <div className="header1">
+          <ul className="nav nav-pills">
+            <li role="presentation" className="active">
+              <Link to="/">Home</Link>
+            </li>
+            <li role="presentation" className="active">
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li role="presentation" className="active">
+              <Link to="/about">About AskUss</Link>
+            </li>
+            <li role="presentation">
+              <a href="#">News</a>
+            </li>
+          </ul>
         </div>
-        )
-    }
+
+        <div className="header2">
+          <nav className="navbar navbar-default">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <Link className="navbar-brand" to="/">
+                  AskUSICT
+                </Link>
+
+                <form className="navbar-form navbar-left" role="search">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Search"
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-default">
+                    Search
+                  </button>
+                </form>
+
+                <ul className="nav navbar-nav">
+                  <li>
+                    <a href="#" onClick={this.handleOpenModal}>
+                      Ask Question
+                    </a>
+                  </li>
+                  <ReactModal
+                    isOpen={this.state.showModal}
+                    contentLabel="ask question modal"
+                    className="Modal"
+                  >
+                    <h1>Ask Your Question Here!</h1>
+                    <form action="" onSubmit={this.handleSubmit}>
+                      <FormInput
+                        label="Question?"
+                        type="text"
+                        name="question"
+                        value={this.state.question}
+                        handleChange={this.handleChange}
+                      />
+                      <FormInput
+                        label="Tags(Optional)"
+                        type="text"
+                        name="question"
+                        value={this.state.question}
+                        handleChange={this.handleChange}
+                      />
+                      <button
+                        className="ask btn btn-primary"
+                        type="submit"
+                        onClick={this.handleCloseModal}
+                      >
+                        Ask
+                      </button>
+                      <button
+                        className="cancel btn btn-warning"
+                        onClick={this.handleCloseModal}
+                      >
+                        Cancel
+                      </button>
+                    </form>
+                  </ReactModal>
+
+                  <li>
+                    <Link to="/login">SignIn</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
