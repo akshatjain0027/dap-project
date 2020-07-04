3cc49632c1095109f76e9dc45064118e3a0c1ae5
PiyushKhurana <khuranapiyush27@gmail.com>
2020-07-04 17:32:17 +0530

    increase token expiry time to 12hrs

--- routes/api/user.js
+++ routes/api/user.js
@@ -99,7 +99,7 @@ router.post("/login", (req, res) => {
         jwt.sign(
           payload,
           keys.secretOrKey,
-          { expiresIn: 3600 },
+          { expiresIn: 43200 },
           (err, token) => {
             res.json({
               success: true,
