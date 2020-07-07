c06e695600eda0eb5146bd937f2b82cb54d88fde
PiyushKhurana <khuranapiyush27@gmail.com>
2020-07-07 13:57:37 +0530

    bookmark ans  untested

--- routes/api/user.js
+++ routes/api/user.js
@@ -17,9 +17,9 @@ const User = require("../../models/user");
 // @desc    Test users route
 // @access  Public
 router.get("/test", (req, res) => {
-  res.json({
-    msg: "users works",
-  });
+	res.json({
+		msg: "users works",
+	});
 });
 
 // @route   POST api/users/register
@@ -27,171 +27,210 @@ router.get("/test", (req, res) => {
 // @access  Public
 // try catch incomplete
 router.post("/register", async (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
+	const { errors, isValid } = validateRegisterInput(req.body);
 
-  //Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  User.findOne({ email: req.body.email }).then(async (user) => {
-    if (user) {
-      errors.email = "Email already exists";
-      return res.status(400).json(errors);
-    } else {
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        avatar: req.body.avatar,
-        password: req.body.password,
-      });
-      const hash = await bcrypt.hash(newUser.password, 10);
-      newUser.password = hash;
-      const user = await newUser.save();
+	//Check validation
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
+	User.findOne({ email: req.body.email }).then(async (user) => {
+		if (user) {
+			errors.email = "Email already exists";
+			return res.status(400).json(errors);
+		} else {
+			const newUser = new User({
+				name: req.body.name,
+				email: req.body.email,
+				avatar: req.body.avatar,
+				password: req.body.password,
+			});
+			const hash = await bcrypt.hash(newUser.password, 10);
+			newUser.password = hash;
+			const user = await newUser.save();
 
-      const payload = {
-        id: user.id,
-        name: user.name,
-        avatar: user.avatar,
-      }; //Create JWT payload
+			const payload = {
+				id: user.id,
+				name: user.name,
+				avatar: user.avatar,
+			}; //Create JWT payload
 
-      //Sign Token
-      const token = await jwt.sign(payload, keys.secretOrKey, {
-        expiresIn: 43200,
-      });
-      res.json({
-        success: true,
-        token: "Bearer " + token,
-      });
-    }
-  });
+			//Sign Token
+			const token = await jwt.sign(payload, keys.secretOrKey, {
+				expiresIn: 43200,
+			});
+			res.json({
+				success: true,
+				token: "Bearer " + token,
+			});
+		}
+	});
 });
 
 // @route   POST api/users/login
 // @desc    Login User / Returning JWT Token
 // @access  Public
 router.post("/login", (req, res) => {
-  const { errors, isValid } = validateLoginInput(req.body);
-  console.log(req.body);
-  //Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  const email = req.body.email;
-  const password = req.body.password;
+	const { errors, isValid } = validateLoginInput(req.body);
+	console.log(req.body);
+	//Check validation
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
+	const email = req.body.email;
+	const password = req.body.password;
 
-  //find user by email
-  User.findOne({ email: email }).then((user) => {
-    if (!user) {
-      errors.email = "User not found";
-      return res.status(404).json(errors);
-    }
-    //Check Password
-    bcrypt.compare(password, user.password).then((isMatch) => {
-      if (isMatch) {
-        //User match
-        const payload = {
-          id: user.id,
-          name: user.name,
-          avatar: user.avatar,
-        }; //Create JWT payload
+	//find user by email
+	User.findOne({ email: email }).then((user) => {
+		if (!user) {
+			errors.email = "User not found";
+			return res.status(404).json(errors);
+		}
+		//Check Password
+		bcrypt.compare(password, user.password).then((isMatch) => {
+			if (isMatch) {
+				//User match
+				const payload = {
+					id: user.id,
+					name: user.name,
+					avatar: user.avatar,
+				}; //Create JWT payload
 
-        //Sign Token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          { expiresIn: 43200 },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token,
-            });
-          }
-        );
-      } else {
-        errors.password = "Password Incorrect";
-        return res.status(400).json(errors);
-      }
-    });
-  });
+				//Sign Token
+				jwt.sign(
+					payload,
+					keys.secretOrKey,
+					{ expiresIn: 43200 },
+					(err, token) => {
+						res.json({
+							success: true,
+							token: "Bearer " + token,
+						});
+					}
+				);
+			} else {
+				errors.password = "Password Incorrect";
+				return res.status(400).json(errors);
+			}
+		});
+	});
 });
 
 // @route   GET api/users/current
 // @desc    Return current user
 // @access  Private
 router.get(
-  "/current",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.json({
-      id: req.user.id,
-      name: req.user.name,
-      email: req.user.email,
-    });
-  }
+	"/current",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		res.json({
+			id: req.user.id,
+			name: req.user.name,
+			email: req.user.email,
+		});
+	}
 );
 
 // @route   POST api/user/bookmark/:id
 // @desc
 // @access  Private
 router.post(
-  "/bookmark/:id",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    const type = req.body.type;
-    const user = await User.findById(req.user.id);
-    if (type === "question") {
-      const quesId = req.params.id;
-      if (
-        user.bookmarked.question.filter(
-          (question) => question.toString() === quesId
-        ).length > 0
-      ) {
-        return res.status(404).json({
-          alreadybookmard:
-            "User already bookmarked this ques , Please Be attentive !!!",
-        });
-      }
+	"/bookmark/:id",
+	passport.authenticate("jwt", { session: false }),
+	async (req, res) => {
+		const type = req.body.type;
+		const user = await User.findById(req.user.id);
 
-      user.bookmarked.question.unshift(quesId);
-    }
-    console.log("Not a question");
-    console.log(type);
-    const user2 = await user.save();
-    res.status(201).json(user2);
-  }
+		if (type === "question") {
+			const quesId = req.params.id;
+			if (
+				user.bookmarked.question.filter(
+					(question) => question.toString() === quesId
+				).length > 0
+			) {
+				return res.status(404).json({
+					alreadybookmard:
+						"User already bookmarked this ques , Please Be attentive !!!",
+				});
+			}
+
+			user.bookmarked.question.unshift(quesId);
+		}
+		if (type === "answer") {
+			const ansId = req.params.id;
+			if (
+				user.bookmarked.answer.filter(
+					(answer) => answer.toString() === ansId
+				).length > 0
+			) {
+				return res.status(404).json({
+					alreadybookmard:
+						"User already bookmarked this ans , Please Be attentive !!!",
+				});
+			}
+
+			user.bookmarked.answer.unshift(ansId);
+		}
+		console.log(type);
+		const user2 = await user.save();
+		res.status(201).json(user2);
+	}
 );
 
 // @route   POST api/user/unbookmark/:id
 // @desc
 // @access  Private
 router.post(
-  "/unbookmark/:id",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    const user = await User.findById(req.user.id);
-    const type = req.body.type;
-    if (type === "question") {
-      const quesId = req.params.id;
+	"/unbookmark/:id",
+	passport.authenticate("jwt", { session: false }),
+	async (req, res) => {
 
-      if (
-        user.bookmarked.question.filter(
-          (question) => question.toString() === quesId
-        ).length === 0
-      ) {
-        return res.status(404).json({
-          alreadybookmard: "Not  bookmarked Yet  , Please Be attentive !!!",
-        });
-      }
-      const removeIndex = user.bookmarked.question
-        .map((item) => item.toString())
-        .indexOf(req.user.id);
+		const user = await User.findById(req.user.id);
+		const type = req.body.type;
 
-      //Splice out of array
-      user.bookmarked.question.splice(removeIndex, 1);
-    }console.log(type);
-    const user2 = await user.save();
-    res.status(201).json(user2);
-  }
+		if (type === "question") {
+			const quesId = req.params.id;
+
+			if (
+				user.bookmarked.question.filter(
+					(question) => question.toString() === quesId
+				).length === 0
+			) {
+				return res.status(404).json({
+					alreadybookmard: "Not  bookmarked Yet  , Please Be attentive !!!",
+				});
+			}
+			const removeIndex = user.bookmarked.question
+				.map((item) => item.toString())
+				.indexOf(req.user.id);
+
+			//Splice out of array
+			user.bookmarked.question.splice(removeIndex, 1);
+		}
+
+		if (type === "answer") {
+			const ansId = req.params.id;
+
+			if (
+				user.bookmarked.answer.filter(
+					(answer) => answer.toString() === ansId
+				).length === 0
+			) {
+				return res.status(404).json({
+					alreadybookmard: "Not  bookmarked Yet  , Please Be attentive !!!",
+				});
+			}
+			const removeIndex = user.bookmarked.answer
+				.map((item) => item.toString())
+				.indexOf(req.user.id);
+
+			//Splice out of array
+			user.bookmarked.answer.splice(removeIndex, 1);
+		}
+
+		console.log(type);
+		const user2 = await user.save();
+		res.status(201).json(user2);
+	}
 );
 
 module.exports = router;
